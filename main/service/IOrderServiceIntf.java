8a5dc117abce2b7642bc94feae219e5d03841228
HimanshiMetta <67338706+HimanshiMetta@users.noreply.github.com>
2021-09-04 15:24:46 +0530

    Update IOrderServiceIntf.java

--- main/service/IOrderServiceIntf.java
+++ main/service/IOrderServiceIntf.java
@@ -5,13 +5,11 @@ import java.util.List;
 
 import com.cg.main.beans.Order;
 
- 
-
-
 public interface IOrderServiceIntf {
-    public Order addOrder(Order order);
+    public Order addOrder(String custId, Order order);
     public  Order removeOrder(Long orderId) ;
     public Order updateOrder(Long orderId,Order order);
     public Order getOrderDetails(Long orderId);
     public List<Order> getAllOrders();
 }
+
