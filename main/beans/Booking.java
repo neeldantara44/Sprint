7f53fdfa11428f84c36d70245ea9b1b2162bd598
HimanshiMetta <67338706+HimanshiMetta@users.noreply.github.com>
2021-09-07 14:58:17 +0530

    Update Booking.java

--- main/beans/Booking.java
+++ main/beans/Booking.java
@@ -1,8 +1,7 @@
 package com.cg.main.beans;
 import java.time.LocalDate;
 import java.time.LocalTime;
-import java.util.Set;
-import java.util.stream.Collector;
+import java.util.List;
 
 import javax.persistence.CascadeType;
 import javax.persistence.Column;
@@ -15,6 +14,9 @@ import javax.persistence.JoinColumn;
 import javax.persistence.OneToMany;
 import javax.persistence.OneToOne;
 import javax.persistence.Table;
+
+
+
 
 @Entity
 @Table
@@ -33,76 +35,62 @@ public class Booking {
 	@OneToOne(fetch = FetchType.EAGER, cascade = CascadeType.MERGE)
     @JoinColumn(name = "userId", referencedColumnName = "id")
 	private Customer customerDetails;
-//	@OneToMany(mappedBy="customerItem")
-//  @JoinColumn(name = "bookingId")
-//	private Set <CustomerItem> oSet;
+	
+	@OneToMany(mappedBy = "bookingId")
+	private List<Item> item;
 	
 	public Booking() {}
-	
-	public Booking(Long bookingId,LocalDate bookingDate,LocalTime bookingTime,String serviceType,Customer customerDetails) {
-		super();
-		this.bookingId= bookingId;
-		this.bookingDate= bookingDate;
-		this.bookingTime= bookingTime;
-		this.serviceType= serviceType;
-		this.customerDetails= customerDetails;
-	}
-
-	public Customer getCustomerDetails() {
-		return customerDetails;
-	}
-
-	public void setCustomerDetails(Customer customerDetails) {
-		this.customerDetails = customerDetails;
-	}
-
-	public Long getBookingId() {
-		return bookingId;
-	}
-	public void setBookingId(Long bookingId) {
-		this.bookingId = bookingId;
-	}
-	public LocalDate getBookingDate() {
-		return bookingDate;
-	}
-	public void setBookingDate(LocalDate bookingDate) {
-		this.bookingDate = bookingDate;
-	}
-	public LocalTime getBookingTime() {
-		return bookingTime;
-	}
-	public void setBookingTime(LocalTime bookingTime) {
-		this.bookingTime = bookingTime;
-	}
-	public String getServiceType() {
-		return serviceType;
-	}
-	public void setServiceType(String serviceType) {
-		this.serviceType = serviceType;
-	}
-	
-	
-//	public Customer getCustomerDetails() {
-//		return customerDetails;
-//	}
-//	public void setCustomerDetails(Customer customerDetails) {
-//		this.customerDetails = customerDetails;
-//	}
-//	
-//	
-//	public Set<CustomerItem> getoSet() {
-//		return oSet;
-//	}
-//	public void setoSet(Set<CustomerItem> oSet) {
-//		this.oSet = oSet;
-//	}
-//	@Override
-//	public String toString() {
-//		return "Booking [bookingId=" + bookingId + ", bookingDate=" + bookingDate + ", bookingTime=" + bookingTime
-//				+ ", serviceType=" + serviceType + ", customerDetails=" + customerDetails + "]";
-//	}
-//	
-//	
-	
+	 
+	public Booking(Long bookingId, LocalDate bookingDate, LocalTime bookingTime, String serviceType,
+		Customer customerDetails, List<Item> item) {
+	super();
+	this.bookingId = bookingId;
+	this.bookingDate = bookingDate;
+	this.bookingTime = bookingTime;
+	this.serviceType = serviceType;
+	this.customerDetails = customerDetails;
+	this.item = item;
+}
+public Long getBookingId() {
+	return bookingId;
+}
+public void setBookingId(Long bookingId) {
+	this.bookingId = bookingId;
+}
+public LocalDate getBookingDate() {
+	return bookingDate;
+}
+public void setBookingDate(LocalDate bookingDate) {
+	this.bookingDate = bookingDate;
+}
+public LocalTime getBookingTime() {
+	return bookingTime;
+}
+public void setBookingTime(LocalTime bookingTime) {
+	this.bookingTime = bookingTime;
+}
+public String getServiceType() {
+	return serviceType;
+}
+public void setServiceType(String serviceType) {
+	this.serviceType = serviceType;
+}
+public Customer getCustomerDetails() {
+	return customerDetails;
+}
+public void setCustomerDetails(Customer customerDetails) {
+	this.customerDetails = customerDetails;
+}
+public List<Item> getItem() {
+	return item;
+}
+public void setItem(List<Item> item) {
+	this.item = item;
+}
+@Override
+public String toString() {
+	return "Booking [bookingId=" + bookingId + ", bookingDate=" + bookingDate + ", bookingTime=" + bookingTime
+			+ ", serviceType=" + serviceType + ", customerDetails=" + customerDetails + ", item=" + item + "]";
+}
 
 }
